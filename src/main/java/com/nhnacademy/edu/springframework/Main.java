06b8f4f8d594adf6e5e1a4fb2dba7ba7605a1379
chaehyonNa <skcogus12@naver.com>
2024-01-31 15:54:57 +0900

    inversionOfControl

--- src/main/java/com/nhnacademy/edu/springframework/Main.java
+++ src/main/java/com/nhnacademy/edu/springframework/Main.java
@@ -5,12 +5,18 @@ import com.nhnacademy.edu.springframework.sender.EmailMessageSender;
 import com.nhnacademy.edu.springframework.sender.MessageSender;
 import com.nhnacademy.edu.springframework.sender.SmsMessageSender;
 import com.nhnacademy.edu.springframework.service.MessageSendService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args){
         User user = new User("user@naver.com", "010-1234-1234");
         String message = "안녕";
-        new MessageSendService(new EmailMessageSender()).doSendMessage(user, message);
-        new MessageSendService(new SmsMessageSender()).doSendMessage(user, message);
+        try(ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml")){
+            MessageSender smsMessageSender = context.getBean("smsMessageSender",MessageSender.class);
+            MessageSender emailMessageSender = context.getBean("emailMessageSender",MessageSender.class);
+
+            smsMessageSender.sendMessage(user, message);
+            emailMessageSender.sendMessage(user, message);
+        }
     }
 }
--- src/main/java/com/nhnacademy/edu/springframework/sender/SmsMessageSender.java
+++ src/main/java/com/nhnacademy/edu/springframework/sender/SmsMessageSender.java
@@ -3,6 +3,12 @@ package com.nhnacademy.edu.springframework.sender;
 import com.nhnacademy.edu.springframework.domain.User;
 
 public class SmsMessageSender implements MessageSender{
+    public void init() {
+        System.out.println("SmsMessageSender init called!!");
+    }
+    public void cleanup() {
+        System.out.println("SmsMessageSender destory called!!");
+    }
     @Override
     public void sendMessage(User user, String message) {
         System.out.println(user.getPhoneNumber() + message);
