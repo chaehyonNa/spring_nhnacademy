826e4dc8dffeba29aa2f261046f624225b8012f9
chaehyonNa <skcogus12@naver.com>
2024-02-02 10:53:21 +0900

    0-2

--- src/main/java/com/nhnacademy/edu/springframework/Main.java
+++ src/main/java/com/nhnacademy/edu/springframework/Main.java
@@ -3,7 +3,7 @@ package com.nhnacademy.edu.springframework;
 import com.nhnacademy.edu.springframework.service.MessageSendService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-//public class Main {
+public class Main {
     public static void main(String[] args){
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("com.nhnacademy.edu.springframework");
         context.getBean("messageSendService", MessageSendService.class).doSendMessage();
