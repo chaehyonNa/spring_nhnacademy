4e6060a70b0e54e5af9390c8dfd4b4fdb1a6cf61
chaehyonNa <skcogus12@naver.com>
2024-02-02 10:21:07 +0900

    doing

--- src/main/java/com/nhnacademy/edu/springframework/Main.java
+++ src/main/java/com/nhnacademy/edu/springframework/Main.java
@@ -1,13 +1,11 @@
 package com.nhnacademy.edu.springframework;
 
 import com.nhnacademy.edu.springframework.service.MessageSendService;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args){
-        try(ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml")){
-            MessageSendService service = context.getBean("messageSendService", MessageSendService.class);
-            service.doSendMessage();
-        }
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("com.nhnacademy.edu.springframework");
+        context.getBean("messageSendService", MessageSendService.class).doSendMessage();
     }
 }
--- src/main/java/com/nhnacademy/edu/springframework/config/MainConfig.java
+++ src/main/java/com/nhnacademy/edu/springframework/config/MainConfig.java
@@ -1,15 +1,28 @@
 package com.nhnacademy.edu.springframework.config;
 
+import com.nhn.dooray.client.DoorayHookSender;
 import com.nhnacademy.edu.springframework.sender.DoorayMessageSender;
 import com.nhnacademy.edu.springframework.sender.MessageSender;
 import com.nhnacademy.edu.springframework.service.MessageSendService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.Scope;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @ImportResource("classpath:/beans.xml")
 public class MainConfig {
+
+    @Value("${hookUrl}")
+    private String hookUrl;
+
+    @Bean
+    @Scope("singleton")
+    public DoorayHookSender doorayHookSender() {
+        return new DoorayHookSender(new RestTemplate(),hookUrl);
+    }
     @Bean
     public MessageSender doorayMessageSender() {
         return new DoorayMessageSender();
